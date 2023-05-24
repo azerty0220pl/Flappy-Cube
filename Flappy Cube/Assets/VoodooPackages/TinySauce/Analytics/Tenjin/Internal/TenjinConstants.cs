c237713c58931966caa27977fd9a13e2d6ca8d67
Szymon Kokot <azerty0220pl@gmail.com>
2023-05-24 12:11:12 +0200

    PlayStore policies update

--- Flappy Cube/Assets/VoodooPackages/TinySauce/Analytics/Tenjin/Internal/TenjinConstants.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Voodoo.Sauce.Internal.Analytics
-{
-    internal static class TenjinConstants
-    {
-        internal const string ApiKey = "S8SPVJNEZGZPZGOYOKR1QSGOS83ZIZOU";
-    }
-}
---
+++ FlappyCube2D(unityProject)/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public GameObject MainMenu;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerPrefs.SetInt("state", 2);
+    }
+
+    private void Update()
+    {
+        switch (PlayerPrefs.GetInt("state"))
+        {
+            case 0:
+                transform.position = new Vector2(0, -3);
+                break;
+
+            case 1:
+                if (transform.position.x < 0)
+                    GetComponent<Rigidbody2D>().AddForce(-transform.right * 3f);
+                else
+                    GetComponent<Rigidbody2D>().AddForce(transform.right * 3f);
+
+                if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+                    if (transform.position.x < 0)
+                        GetComponent<Rigidbody2D>().AddForce(transform.right * 4, ForceMode2D.Impulse);
+                    else
+                        GetComponent<Rigidbody2D>().AddForce(-transform.right * 4, ForceMode2D.Impulse);
+                }
+                break;
+
+            case 2:
+                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                MainMenu.SetActive(true);
+                break;
+        }
+    }
+}
